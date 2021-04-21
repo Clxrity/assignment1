867a5ffac1d27cdcedd62e726e11ac41b46dcf62
Clxrity <81974054+Clxrity@users.noreply.github.com>
2021-04-21 11:59:47 -0400

    Updated some of the checcking info

--- src/main/java/com/meritamerica/assignment1/CheckingAccount.java
+++ src/main/java/com/meritamerica/assignment1/CheckingAccount.java
@@ -2,4 +2,25 @@ package com.meritamerica.assignment1;
 
 public class CheckingAccount {
 	
+	private double balance;
+	
+	//Opening/Starting balance 0.0
+	public CheckingAccount() {
+		balance = 0.0;
+	}
+	
+	//Deposit in checkings
+	public void depositChecking(double amount) {
+		balance = balance + amount;
+	}
+	
+	//Withdraw from checkings
+	public void withdrawChecking(double amount) {
+		balance = balance - amount;
+	}
+	
+	//Get Checking Balance
+	public double getBalance() {
+		return balance;
+	}
 }
