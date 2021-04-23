5d11585c16fc3c1c633d148e8e9434265d0e0635
Clxrity <81974054+Clxrity@users.noreply.github.com>
2021-04-23 17:49:37 -0400

    Update MeritAmericaBankApp.java

--- src/main/java/com/meritamerica/assignment1/MeritAmericaBankApp.java
+++ src/main/java/com/meritamerica/assignment1/MeritAmericaBankApp.java
@@ -2,6 +2,9 @@ package com.meritamerica.assignment1;
 
 public class MeritAmericaBankApp extends SavingsAccount {
 	
+	CheckingAccount checking = new CheckingAccount();
+	SavingsAccount savings = new SavingsAccount();
+	
 	public static void main(String[] args) {
 		
 		
